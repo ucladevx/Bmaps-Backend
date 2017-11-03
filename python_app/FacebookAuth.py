b64d55ef926e32da70a8b59718fc52ef67fb92eb
Katrina Wijaya <katrina@thewijayas.com>
2017-11-03 05:41:00 -0700

    Cleaned up code and added some comments

--- python_app/FacebookAuth.py
+++ python_app/FacebookAuth.py
@@ -1,16 +1,21 @@
+# Facebook Authentication
+# TODO: logout, store user profiles
+
 from flask import Flask, redirect, url_for, session, request, Blueprint
 from flask_oauth import OAuth
- 
+
+# Got APP_ID and APP_SECRET from Mappening app with developers.facebook.com
 DEBUG = True
 SECRET_KEY = 'development key'
 FACEBOOK_APP_ID = '353855031743097'
 FACEBOOK_APP_SECRET = '2831879e276d90955f3aafe0627d3673'
- 
-fb_auth_api = Blueprint('fb_auth_api', __name__)
-fb_auth_api.debug = DEBUG
-fb_auth_api.secret_key = SECRET_KEY
+
+fb_auth = Blueprint('fb_auth', __name__)
+fb_auth.debug = DEBUG
+fb_auth.secret_key = SECRET_KEY
 oauth = OAuth()
 
+# OAuth for authentication. Also supports Google Authentication.
 facebook = oauth.remote_app('facebook',
     base_url='https://graph.facebook.com/',
     request_token_url=None,
@@ -25,29 +30,30 @@ facebook = oauth.remote_app('facebook',
 def get_facebook_oauth_token():
     return session.get('oauth_token')
 
-@fb_auth_api.route('/login')
+@fb_auth.route('/login')
 def login():
-    return redirect(url_for('fb_auth_api.facebook_login'))
+    return redirect(url_for('fb_auth.facebook_login'))
 
-@fb_auth_api.route('/login/facebook')
+@fb_auth.route('/login/facebook')
 def facebook_login():
     return facebook.authorize(
-      callback=url_for('fb_auth_api.facebook_authorized',
+      callback=url_for('fb_auth.facebook_authorized',
       next=request.args.get('next') or None, _external=True))
 
-
-@fb_auth_api.route('/login/authorized')
+# Checks whether authentication works or access is denied
+# If successful, returns some user data
+@fb_auth.route('/login/authorized')
 @facebook.authorized_handler
 def facebook_authorized(resp):
     if resp is None or 'access_token' not in resp:
         flash('Access denied: reason=%s error=%s' % (
             request.args['error_reason'],
             request.args['error_description']))
-        return redirect(url_for('fb_auth_api.login'))
+        return redirect(url_for('fb_auth.login'))
     session['oauth_token'] = (resp['access_token'], '')
 
     me = facebook.get('/me')
     return 'Logged in as id=%s name=%s' % (me.data['id'], me.data['name'])
 
 if __name__ == '__main__':
-    fb_auth_api.run()
+    fb_auth.run()
