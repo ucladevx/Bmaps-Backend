e45b2eb2634e6eb8befefa4cf9ae8dae0cd2d63f
Jason Xu <xu.jason.jx@gmail.com>
2017-11-24 21:32:21 -0800

    update retrieval functions with error checking for missing fields and consolidation of getter functions

--- python_app/events.py
+++ python_app/events.py
@@ -28,9 +28,24 @@ client = pymongo.MongoClient(uri)
 db = client['mappening_data'] 
 events_collection = db.map_events
 
+"""
+CHANGES
+when search for category, just put actual name ('EVENT' not needed)
+everything calls find_events_in_database, giving search terms and options if needed
+
+in processing data from database:
+'venue' is now 'place' and has less info: name, id, and location
+'attendance' --> 'stats' contains attending_count, interested_count, and maybe_count
+'end_time' may not be set
+'is_canceled' may return False boolean value
+defaults set by using dict.get(key, default value), returns None (null) if no default value given
+"""
+
 # Returns JSON of all events in format that Mapbox likes
 @Events.route('/api/events', methods=['GET'])
 def get_all_events():
+    return find_events_in_database()
+    """
     output = []
     for event in events_collection.find():
       print ("Event: " + event["name"].encode('ascii', 'ignore'))
@@ -39,8 +54,8 @@ def get_all_events():
         'type': 'Feature',
         'geometry': {
             'coordinates': [
-                event['venue']['location']['longitude'],
-                event['venue']['location']['latitude']
+                event['place']['location']['longitude'],
+                event['place']['location']['latitude']
             ],
             'type': 'Point'
         },
@@ -49,7 +64,7 @@ def get_all_events():
             'description': event['description'],
             'start_time': event['startTime'],
             'end_time': event['endTime'],
-            'venue': event['venue'],
+            'venue': event['place'],
             'stats': event['stats'],
             'category': event['category'],
             'cover_picture': event['coverPicture'],
@@ -59,6 +74,7 @@ def get_all_events():
         }
       })
     return jsonify({'features': output, 'type': 'FeatureCollection'})
+    """
 
 # Returns JSON of matching event names
 @Events.route('/api/search/<search_term>', methods=['GET'])
@@ -77,6 +93,8 @@ def get_events_for_search(search_term):
 # /<> defaults to strings without any slashes
 @Events.route('/api/event-name/<event_name>', methods=['GET'])
 def get_event_by_name(event_name):
+    return find_events_in_database('name', event_name, True)
+    """
     output = []
     event = events_collection.find_one({'name': event_name})
     if event:
@@ -85,8 +103,8 @@ def get_event_by_name(event_name):
         'type': 'Feature',
         'geometry': {
             'coordinates': [
-                event['venue']['location']['longitude'],
-                event['venue']['location']['latitude']
+                event['place']['location']['longitude'],
+                event['place']['location']['latitude']
             ],
             'type': 'Point'
         },
@@ -95,7 +113,7 @@ def get_event_by_name(event_name):
             'description': event['description'],
             'start_time': event['startTime'],
             'end_time': event['endTime'],
-            'venue': event['venue'],
+            'venue': event['place'],
             'stats': event['stats'],
             'category': event['category'],
             'cover_picture': event['coverPicture'],
@@ -107,10 +125,13 @@ def get_event_by_name(event_name):
     else:
       return "No event of name '{}'".format(event_name)
     return jsonify({'features': output, 'type': 'FeatureCollection'})
+    """
 
 # Returns JSON of singular event by event id
 @Events.route('/api/event-id/<event_id>', methods=['GET'])
 def get_event_by_id(event_id):
+    return find_events_in_database('id', event_id, True)
+    """
     output = []
     event = events_collection.find_one({'id': event_id})
     if event:
@@ -119,8 +140,8 @@ def get_event_by_id(event_id):
         'type': 'Feature',
         'geometry': {
             'coordinates': [
-                event['venue']['location']['longitude'],
-                event['venue']['location']['latitude']
+                event['place']['location']['longitude'],
+                event['place']['location']['latitude']
             ],
             'type': 'Point'
         },
@@ -129,7 +150,7 @@ def get_event_by_id(event_id):
             'description': event['description'],
             'start_time': event['startTime'],
             'end_time': event['endTime'],
-            'venue': event['venue'],
+            'venue': event['place'],
             'stats': event['stats'],
             'category': event['category'],
             'cover_picture': event['coverPicture'],
@@ -141,10 +162,17 @@ def get_event_by_id(event_id):
     else:
       return "No event of id '{}'".format(event_id)
     return jsonify({'features': output, 'type': 'FeatureCollection'})
+    """
 
 # Returns JSON of events by event category
+# Event category examples: food, THEATER
+# use regexes to search in 'category', since both EVENT_TYPE and TYPE_EVENT string formats exist now
 @Events.route('/api/event-category/<event_category>', methods=['GET'])
 def get_event_by_category(event_category):
+    regex_str = '^{0}|{0}$'.format(event_category.upper())
+    cat_regex_obj = re.compile(regex_str)
+    return find_events_in_database('category', cat_regex_obj)
+    """
     output = []
     events_cursor = events_collection.find({'category': event_category})
     if events_cursor.count() > 0:
@@ -154,8 +182,8 @@ def get_event_by_category(event_category):
             'type': 'Feature',
             'geometry': {
                 'coordinates': [
-                    event['venue']['location']['longitude'],
-                    event['venue']['location']['latitude']
+                    event['place']['location']['longitude'],
+                    event['place']['location']['latitude']
                 ],
                 'type': 'Point'
             },
@@ -164,7 +192,7 @@ def get_event_by_category(event_category):
                 'description': event['description'],
                 'start_time': event['startTime'],
                 'end_time': event['endTime'],
-                'venue': event['venue'],
+                'venue': event['place'],
                 'stats': event['stats'],
                 'category': event['category'],
                 'cover_picture': event['coverPicture'],
@@ -176,10 +204,13 @@ def get_event_by_category(event_category):
     else:
         return "No event(s) of category '{}'".format(event_category)
     return jsonify({'features': output, 'type': 'FeatureCollection'})
+    """
 
 # Returns JSON of events with free food
 @Events.route('/api/event-food', methods=['GET'])
 def get_event_by_food():
+    return get_event_by_category('food')
+    """
     output = []
     events_cursor = events_collection.find({'category': 'EVENT_FOOD'})
     if events_cursor.count() > 0:
@@ -211,16 +242,72 @@ def get_event_by_food():
     else:
         return "No event(s) with free food"
     return jsonify({'features': output, 'type': 'FeatureCollection'})
+    """
+
+# find_key / value = search strings, can pass in REGEX objects for find_value (using re.compile)
+def find_events_in_database(find_key='', find_value='', one_result_expected=False, print_results=False):
+    output = []
+    # for getting all events, no search query needed (empty dict)
+    search_pair = {}
+    if find_key and find_value:
+        search_pair[find_key] = find_value
+
+    if one_result_expected:
+        single_event = events_collection.find_one({find_key: find_value})
+        if single_event:
+            output.append(process_event_info(single_event))
+            if print_results:
+                print('Event: {0}'.format(single_event.get('name', 'No Name')))
+        else:
+            return 'Cannot find single event with attribute {0}: value {1}'.format(find_key, find_value)
+    else:
+        events_cursor = events_collection.find({find_key: find_value})
+        if events_cursor.count() > 0:
+            for event in events_cursor:
+                output.append(process_event_info(event))
+                if print_results:
+                    print('Event: {0}'.format(event.get('name', 'No Name')))
+        else:
+            return 'Cannot find multiple events with attribute {0}: value {1}'.format(find_key, find_value)
+    return jsonify({'features': output, 'type': 'FeatureCollection'})
+
+def process_event_info(event):
+    formatted_info = {
+        # will ALWAYS have an ID
+        'id': event['id'],
+        'type': 'Feature',
+        'geometry': {
+            # no coordinates? default to Bruin Bear
+            'coordinates': [
+                event['place']['location'].get('longitude', event_caller.CENTER_LONGITUDE),
+                event['place']['location'].get('longitude', event_caller.CENTER_LATITUDE)
+            ],
+            'type': 'Point'
+        },
+        'properties': {
+            'event_name': event.get('name', 'No Name'), 
+            'description': event.get('description', 'No Description'),
+            'start_time': event.get('start_time', 'Unknown Start Time'),
+            'end_time': event.get('end_time', 'No End Time'),
+            'venue': event['place'],
+            'stats': event['attendance'],    
+            'category': event.get('category', 'No Category Chosen'),
+            'cover_picture': event['cover'].get('source', 'No Cover Image') if 'cover' in event else 'No Cover Info',
+            'is_cancelled': event.get('is_canceled', False),
+            'ticketing': event.get('ticketing', 'No Ticketing Info'),
+            'free_food': 'YES' if 'category' in event and 'FOOD' in event['category'] else 'NO'
+        }
+    }
+    return formatted_info
 
 # Get all UCLA-related Facebook events and add to database
-# TODO: Don't add duplicates, error checking
 @Events.route('/api/populate-ucla-events-database')
 def populate_ucla_events_database():
     # Location of Bruin Bear
     # current_events = get_facebook_events(34.070964, -118.444757)
 
     # DUMB WAY to refresh database: delete all data, then insert all new
-    # GOAL: update data already there, insert new, delete leftover data (not in new events)
+    # TODO: update data already there, insert new, delete leftover data (not in new events)
     delete_result = events_collection.delete_many({})
 
     current_events = event_caller.get_facebook_events()
