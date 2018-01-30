25aa79b22f3308d725e9cae92dca7755231b4930
Jorge Fuentes <jsjfuentesj@yahoo.com>
2018-01-29 22:29:06 -0800

    Add base for data and preprocessing

--- python_app/mlcategories.py
+++ python_app/mlcategories.py
@@ -1,23 +1,55 @@
-from pymongo import MongoClient
-import os
-from dotenv import load_dotenv
+from ourDb import events_collection, total_events_collection
+import re
+import nltk
+import string
 
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-load_dotenv(dotenv_path)
+from gensim.models import Phrases
+from nltk.corpus import wordnet as wn
 
-# Standard URI format: mongodb://[dbuser:dbpassword@]host:port/dbname
-MLAB_USERNAME = os.getenv('MLAB_USERNAME')
-MLAB_PASSWORD = os.getenv('MLAB_PASSWORD')
+class PreprocessText:
+    def matchNotX(strg, search=re.compile(r'[^!#$%&()*+,-./:;<=>?@\\^_}|{~0123456789]').search):
+        """make sure word has something than punctuation"""
+        return bool(search(strg)) #make sure word has something other than punctuation
 
-uri = 'mongodb://{0}:{1}@ds044709.mlab.com:44709/mappening_data'.format(MLAB_USERNAME, MLAB_PASSWORD)
+    def preprocess(texts):
+        stoplist = set('for a of the and to in . / '.split())
+        stoplist = set(nltk.corpus.stopwords.words('english')) | stoplist | set(string.punctuation)
+        return [[word.strip(string.punctuation).lower() for word in document if word not in stoplist and matchNotX(word)] for document in texts]
 
-# Set up database connection
+    ##generating phrases
+    def getPhrases(texts):
+        texts = getMedTerms(texts)
+        bigram = Phrases(texts, min_count=10)
+        return bigram
 
-# Set up database connection.
-client = MongoClient(uri)
-db = client['mappening_data']
-events_collection = db.map_events
-total_events_collection = db.total_events
 
-for e in events_collection.find():
-    print(e)
+    def topBigrams(texts, n, tri=False):
+        flatTexts = []
+        for text in texts:
+            for word in text:
+                flatTexts.append(word)
+        bigram_measures = nltk.collocations.BigramAssocMeasures()
+        trigram_measures = nltk.collocations.TrigramAssocMeasures()
+        topAnswers = []
+        if tri:
+            finder = nltk.collocations.TrigramCollocationFinder.from_words(flatTexts)
+            finder.apply_freq_filter(7)
+            return finder.nbest(trigram_measures.pmi, n)
+        else:
+            finder = nltk.collocations.BigramCollocationFinder.from_words(flatTexts)
+            finder.apply_freq_filter(7)
+            return finder.nbest(bigram_measures.pmi, n)
+
+def gatherCategorizedEvents():
+    allCategorizedEvents = []
+    allEvents = total_events_collection.find({}, {"category": 1, "description": 1, "name": 1})
+    count = 0
+    for e in allEvents:
+        count += 1
+        if e.get('category', None) != None:
+            allCategorizedEvents.append(e)
+    print count, "events and using the", len(allCategorizedEvents), "categorized events"
+    return allCategorizedEvents
+
+X = gatherCategorizedEvents()
+print(X)
