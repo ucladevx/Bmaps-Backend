936a4af975daf9cdcf60ece1536d9f744c773169
Katrina Wijaya <katrina@thewijayas.com>
2018-02-14 04:39:07 -0800

    Small changes to tokenizer to add back amp and apos symbols and make case insensitive with stop words

--- python_app/process.py
+++ python_app/process.py
@@ -10,7 +10,7 @@ nltk.download('punkt')
 import re
 import string
 from tqdm import tqdm
-from nltk.tokenize.moses import MosesTokenizer
+from nltk.tokenize.moses import MosesTokenizer, MosesDetokenizer
 
 from nltk.corpus import wordnet as wn
 
@@ -19,27 +19,30 @@ Process = Blueprint('Process', __name__)
 # Enable Cross Origin Resource Sharing (CORS)
 cors = CORS(Process)
 
-# @Process.route('/api/process/<text>', methods=['GET'])
+@Process.route('/api/process/<text>', methods=['GET'])
 def processText(text):
     tokenizer = MosesTokenizer() #tokenizers are basically an advanced split
+    detokenizer = MosesDetokenizer()
 
     # text = "testing this. UCLA ackerman for Boelter 3400 why engr 4 at UCLA. Rstaurat of Los Angeles LA, Room 4760 @ the place - UCLA"
-    print "Original text: " + text
+    # print "Original text: " + text
 
     processed_text = tokenizer.tokenize(text)
+
+    # print "Tokenized text: " + " ".join(processed_text)
+
+    # Need to detokenize to get all the weird symbols back as symbols
+    # Am I doing this badly
+    # Wanted to get & not amp and ' not apos'
+    processed_text = detokenizer.detokenize(processed_text)
+
+    # print "Detokenized text: " + " ".join(processed_text)
+
     processed_text = preprocess(processed_text)
 
-    print "Processed text: " + " ".join(processed_text)
-    return " ".join(processed_text) #jsonify(processed_text)
+    # print "Processed text: " + " ".join(processed_text)
 
-# [
-#   "testing", 
-#   "ackerman", 
-#   "boelter", 
-#   "engr", 
-#   "rstaurat", 
-#   "place"
-# ]
+    return " ".join(processed_text) #jsonify(processed_text)
 
 def matchNotX(strg, search=re.compile(r'[^!#$%&()*+,-./:;<=>?@\\^_}|{~0123456789]').search):
     """make sure word has something than punctuation"""
@@ -48,6 +51,7 @@ def matchNotX(strg, search=re.compile(r'[^!#$%&()*+,-./:;<=>?@\\^_}|{~0123456789
 def preprocess(text):
     """Remove all useless words and punct, make lowercase"""
     # Dorm Dormitory Building Bldg Ofc Office 
-    stoplist = set('for a of the and to in . / center field plaza residential los ucla angeles la westwood room dormitory dorm building bldg ofc office hall the @ at - ,'.split())
+    # TODO why is it case sensitive wtf
+    stoplist = set('for a of the and to in . / center field plaza residential los ucla angeles la westwood room dormitory dorm building bldg ofc office hall the & @ at - ,'.split())
     stoplist = set(nltk.corpus.stopwords.words('english')) | stoplist | set(string.punctuation)
-    return [word.strip(string.punctuation).lower() for word in text if word not in stoplist and matchNotX(word)]    
+    return [word.strip(string.punctuation).lower() for word in text if word.lower() not in stoplist and matchNotX(word)]    
