d1341bf509ea97a9b075de53a0f5ddfb52e83516
Katrina Wijaya <katrina@thewijayas.com>
2018-03-07 02:22:14 -0800

    Working but slightly buggy studious scraper

--- scraping/happenings/happenings.py
+++ scraping/happenings/happenings.py
@@ -54,55 +54,66 @@ def getEventInfo():
 
     # Get the event information
     event_title = soup.find('h2')
-    if debugMode: print "TITLE: " + event_title.text.strip() + '\n'
-    data[counter]['event_name'] = event_title.text.strip()
-    
+    data[counter]['event_name'] = ""
+    if event_title:
+      if debugMode: print "TITLE: " + event_title.text.strip() + '\n'
+      data[counter]['event_name'] = event_title.text.strip()
+
     event_date_location = soup.find('p', attrs={'id': 'more-info'})
-    if debugMode: print "DATE/LOCATION: " 
-    if debugMode: print event_date_location.contents[0].strip()
-    if debugMode: print event_date_location.contents[1].strip() + '\n'
-    data[counter]['date'] = event_date_location.contents[0].strip()
-    split = event_date_location.contents[1].split(",")
-    data[counter]['time'] = split[0].strip()
-    data[counter]['location'] = split[1].strip()
+    data[counter]['date'] = ""
+    data[counter]['time'] = ""
+    data[counter]['location'] = ""
+    if event_date_location:
+      if debugMode: print "DATE/LOCATION: " 
+      if debugMode: print event_date_location.contents[0].strip()
+      if debugMode: print event_date_location.contents[1].strip() + '\n'
+      data[counter]['date'] = event_date_location.contents[0].strip()
+      split = event_date_location.contents[1].split(",")
+      data[counter]['time'] = split[0].strip()
+      data[counter]['location'] = split[1].strip()
 
     event_admission = soup.find('h3', text='Admission')
-    if debugMode: print "ADMISSION: " + event_admission.next_sibling.text + '\n'
-    data[counter]['admission'] = event_admission.next_sibling.text.strip()
+    data[counter]['admission'] = ""
+    if event_admission:
+      if debugMode: print "ADMISSION: " + event_admission.next_sibling.text + '\n'
+      data[counter]['admission'] = event_admission.next_sibling.text.strip()
 
     # http://happenings.ucla.edu/all/event/242683
     event_contact = soup.find('h3', text='Contact')
-    if debugMode: print "CONTACT: "
     data[counter]['contact'] = []
-    for content in event_contact.next_sibling.contents:
-      if content.name == 'a' and content.get('href', ''):
-        content_re = re.sub('mailto:','', content['href'])
-        if debugMode: print content_re
-        data[counter]['contact'].append(content_re)
-      else:
-        if debugMode: print content
-        data[counter]['contact'].append(content)
-    if debugMode: print '\n'
+    if event_contact:
+      if debugMode: print "CONTACT: "
+      for content in event_contact.next_sibling.contents:
+        if content.name == 'a' and content.get('href', ''):
+          content_re = re.sub('mailto:','', content['href'])
+          if debugMode: print content_re
+          data[counter]['contact'].append(content_re)
+        else:
+          if debugMode: print content
+          data[counter]['contact'].append(content)
+      if debugMode: print '\n'
 
     event_info = soup.find('h3', text='Additional Information')
-    if debugMode: print "INFORMATION: "
     data[counter]['info'] = ""
+    if event_info:
+      if debugMode: print "INFORMATION: "
+      shouldPrint = False
+      event_details = soup.find_all('div', attrs={'id': 'event-details'})
+      if event_details:
+        for tag in event_details:
+          tags = tag.find_all(['p', 'li'])
+          if tags:
+            for t in tags:
+              if t.text == event_info.next_sibling.text:
+                shouldPrint = True
+              if shouldPrint and t.name == "li":
+                if debugMode: print " - " + t.text.strip()
+                data[counter]['info'] = data[counter]['info'] + "\n - " + t.text.strip()
+              elif shouldPrint:
+                if debugMode: print t.text.strip()
+                data[counter]['info'] = data[counter]['info'] + "\n" + t.text.strip()
 
-    shouldPrint = False
-    event_details = soup.find_all('div', attrs={'id': 'event-details'})
-    for tag in event_details:
-      tags = tag.find_all(['p', 'li'])
-      for t in tags:
-        if t.text == event_info.next_sibling.text:
-          shouldPrint = True
-        if shouldPrint and t.name == "li":
-          if debugMode: print " - " + t.text.strip()
-          data[counter]['info'] = data[counter]['info'] + "\n - " + t.text.strip()
-        elif shouldPrint:
-          if debugMode: print t.text.strip()
-          data[counter]['info'] = data[counter]['info'] + "\n" + t.text.strip()
-
-    if debugMode: print "~~~~~~~~~~"
+    print "~~~~~ " + str(counter) + " ~~~~~"
     counter += 1
 
   with open('happeningsEvents.json', 'w') as outfile:
