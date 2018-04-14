6e6759336f72c636b496288ae372fd7e741e2687
Katrina Wijaya <katrina@thewijayas.com>
2018-04-13 17:55:04 -0700

    Added extension for github pages to host our documentation

--- docs/conf.py
+++ docs/conf.py
@@ -40,7 +40,7 @@ autodoc_mock_imports = [
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.githubpages']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
