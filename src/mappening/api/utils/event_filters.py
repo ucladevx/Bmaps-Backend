58ad8ada433f1dbbc0655247145096a41c48b70e
Katrina Wijaya <katrina@thewijayas.com>
2018-04-16 16:43:52 -0700

    Starting filter implementation

---
+++ src/mappening/api/utils/event_filters.py
@@ -0,0 +1,32 @@
+from mappening.utils.database import events_current_collection
+
+from flask import jsonify
+import requests
+import json
+import time, datetime, dateutil.parser, pytz
+from dateutil.tz import tzlocal
+
+def filter_by_happening_now():
+  print("Hello")
+
+def filter_by_upcoming():
+  print("Hello")
+
+def filter_by_morning():
+  print("Hello")
+
+def filter_by_afternoon():
+  print("Hello")
+
+def filter_by_night():
+  print("Hello")
+
+def filter_by_campus():
+  print("Hello")
+
+def filter_by_popular():
+  print("Hello")
+
+def filter_by_free_food():
+  print("Hello")
+
