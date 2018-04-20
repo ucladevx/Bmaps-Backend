e6408cc45600d9596e31dc163c83b87edfd64b36
Jason Xu <xu.jason.jx@gmail.com>
2018-04-20 16:42:03 -0700

    merge in all the log stats changes, to catch up (UNTESTED since API broke)

--- src/app.py
+++ src/app.py
@@ -1,5 +1,23 @@
+# run this before anything else: checks for command line arguments
+# default: no arguments, liked when using Makefile (normal API backend running)
+import argparse
+parser = argparse.ArgumentParser()
+# to turn option into flag, use action= parameter: calls a predefined function
+# store_true is one of many default functions for action=, later can check args.test = True
+parser.add_argument('-t', '--test', help='Use a test database, to protect live data.', action='store_true')
+parser.add_argument('-d', '--days-before', help='Specify # of days to go back in time for past events.', type=int)
+parser.add_argument('-c', '--clear', help='Clear out old database data to start anew.', action='store_true')
+parser.add_argument('-p', '--refresh-pages', help='Run a search for any new pages to look through.', action='store_true')
+args = parser.parse_args()
+
+# if ever need to quit early, call sys.exit()
+import sys
+
+# there's an 'app' Flask object in mappening's __init__.py, which also links to blueprints to other modules
+
 from mappening import app
 from mappening.utils import scheduler
+from mappening.api.utils import event_utils
 
 from flask import Flask
 
@@ -10,18 +28,32 @@ def index():
     return "Mappening is running!"
 
 if __name__ == "__main__":
+    print('Arguments passed: {0}'.format(args))
+    # sys.exit()
+
     # Another thread to run the periodic events update, daily
     event_update_thread = Thread(target = scheduler.event_thread_func)
     event_update_thread.start()
 
-    code_update_date = "2/15/18"
+    code_update_date = "4/20/18"
     print("Updated on: {0}".format(code_update_date))
 
-    print('Mappening is happening!')
+    print("UPDATE EVENTS FIRST...\n")
+    dbit = args.days_before
+    # pass in args from command line, need to check it's there
+    if not dbit or dbit < 1:
+        dbit = 0
+    event_utils.update_ucla_events_database(use_test=args.test,
+                                            days_back_in_time=dbit,
+                                            clear_old_db=args.clear,
+                                            refresh_pages=args.refresh_pages)
+
+    # GOOD TO KNOW
+    # to QUIT when not in Docker container: run Ctrl+\ (SIGQUIT, equivalent to kill -3 <pid>)
+    # FORCE QUIT: Ctrl-Z to pause + put in background + add to 'jobs' list
+    # THEN kill -9 %1 to UNCONDITIONALLY eliminate job #1, which is this python script
 
     # Flask defaults to port 5000
     # If debug is true, runs 2 instances at once (so two copies of all threads)
     app.run(host='0.0.0.0', debug=False)
-
-    
 
