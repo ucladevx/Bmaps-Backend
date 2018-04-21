dfb8adb0793b3adf420e6132f27e0051235fe6be
Katrina Wijaya <katrina@thewijayas.com>
2018-04-20 20:45:40 -0700

    WHY DOESN'T THIS WORK

--- src/mappening/api/utils/event_filters.py
+++ src/mappening/api/utils/event_filters.py
@@ -1,10 +1,12 @@
 from mappening.utils.database import events_current_collection
+from mappening.api.utils import event_utils
 
 from flask import Flask, jsonify, request, json, Blueprint
 import requests
 import json
-import time, datetime, dateutil.parser, pytz
-from dateutil.tz import tzlocal
+from datetime import datetime, timedelta
+import pytz
+from pytz import timezone
 
 # Route Prefix: /api/v2/events
 event_filters = Blueprint('event_filters', __name__)
@@ -19,7 +21,7 @@ def filter_events():
     :param when: An optional query component/parameter that specifies whether an event is happening now (event start time <= current time < event end time), is an upcoming event (event start time <= current time + 2 hours), or allows you to specify a time period with the `time` parameter. The parameter values can be `now`, `upcoming`, or `time`.
     :type term: str or None
 
-    :param time: An optional query component/parameter that is only checked (and must be set) if the parameter `when` was set to value `time`. May have value `morning`, `afternoon`, or `night` where `morning` is from 3 am - 12 pm, `afternoon` is from 12 pm - 5 pm, and `night` is from 5 pm - 3 am. The start times are inclusive while the end times are exclusive.
+    :param time: An optional query component/parameter that is only checked (and must be set) if the parameter `when` was set to value `time`. May have value `morning`, `afternoon`, or `night` where `morning` is from 3 am - 12 pm, `afternoon` is from 12 pm - 5 pm, and `night` is from 5 pm - 3 am. The start times are inclusive while the end times are exclusive. May have *multiple* values such as in example route above. Will return events that are in the morning or afternoon time period.
     :type date: str or None
 
     :param where: An optional query component/parameter that specifies a location filter for events. The parameter values can be `nearby`, `oncampus`, or `offcampus` where `nearby` filters for events within a TODO radius, `oncampus` gets locations within the UCLA boundary, and `offcampus` gets locations in Westwood and outside of the UCLA boundaries.
@@ -45,46 +47,51 @@ def filter_events():
     search_dict = {}
     output = []
 
-    # Add to search dict
+    # Add to search dict 
+    # Time filtering
     if when:
       if when == 'now':
-        filter_by_happening_now()
+        filter_by_happening_now(search_dict)
       elif when == 'upcoming':
-        filter_by_upcoming()
+        filter_by_upcoming(search_dict)
       elif when == 'time':
         if time:
-          filter_by_time(time)
+          filter_by_time(search_dict, time)
           return jsonify({'time': time})
         else:
           return 'Expected time period to be set!'
       else:
         return 'Invalid value passed to `when` parameter.'
 
+    # Location filtering
     if where:
       if where == 'nearby':
-        filter_by_nearby()
+        filter_by_nearby(search_dict)
       elif where == 'oncampus':
-        filter_by_oncampus()
+        filter_by_oncampus(search_dict)
       elif where == 'offcampus':
-        filter_by_offcampus()
+        filter_by_offcampus(search_dict)
       else:
         return 'Invalid value passed to `where` parameter.'
 
+    # Other filters
     if popular and popular.lower() == "true":
         if popular_threshold and popular_threshold.lower() == "true":
-          filter_by_popular(popular_threshold)
+          filter_by_popular(search_dict, popular_threshold)
         else:
-          filter_by_popular()
+          filter_by_popular(search_dict)
 
     if food and food.lower() == "true":
-      filter_by_free_food()
+      filter_by_free_food(search_dict)
       
-    return 'Success!'
+    # return 'Success!'
+
+    return event_utils.find_events_in_database(search_dict)
 
 
     # if term:
     #     term_regex = re.compile('.*' + term + '.*', re.IGNORECASE)
-    #     search_dict["$or"] = [ {"name":term_regex}, {"description":term_regex} ] # MongoDB's syntax for find name in name or description
+    #     search_dict["$or"] = [ {"name":term_regex}, {"description":term_regex} ] 
     # if date:
     #     date_regex = event_utils.construct_date_regex(date)
     #     search_dict['start_time'] = date_regex
@@ -96,23 +103,29 @@ def filter_events():
     # return event_utils.find_events_in_database(search_dict)
 
 # Get current time and get all events whose start time <= current time < end time
-def filter_by_happening_now():
+def filter_by_happening_now(search_dict):
   print("filter_by_happening_now")
 
+  now = pytz.timezone('America/Los_Angeles').localize(datetime.now())
+  now = datetime.strftime(now, '%a, %d %b %Y %H:%M:%S')
+  search_dict['$and'] = [ {"start_time":{"$lte": now}}, {"end_time":{"$gt": now}} ]
+
 # Get current time and get all events whose start time is <= 2 hours for now
-def filter_by_upcoming():
+def filter_by_upcoming(search_dict):
   print("filter_by_upcoming")
+  # in_two_hours = datetime.now() + timedelta(hours=2)
+  # search_dict['start_time'] = {"$lte": in_two_hours}
 
 # Morning = events whose start time is >= 3 am and < 12 pm
 # Afternoon = events whose start time is >= 12 pm and < 5 pm
 # Night = events whose start time is >= 5 pm and < 3 am
-def filter_by_time(time_period):
+def filter_by_time(search_dict, time_period):
   print("filter_by_time")
 
 # Using Facebook event statistics, sort by the number of people interested.
 # Enable specification of top # of events to return or a threshold
 # If (interested || going > 100) then popular
-def filter_by_popular(threshold=None):
+def filter_by_popular(search_dict, threshold=None):
   if threshold:
     print("filter_by_popular theshold")
   else:
@@ -120,21 +133,21 @@ def filter_by_popular(threshold=None):
 
 # Get event location and check whether coordinates are within the UCLA boundary
 # as specified by ucla_border.geojson
-def filter_by_oncampus():
+def filter_by_oncampus(search_dict):
   print("filter_by_oncampus")
 
 # Get event location and check whether coordinates are within the UCLA boundary
 # as specified by ucla_border.geojson. If NOT, then use.
-def filter_by_offcampus():
+def filter_by_offcampus(search_dict):
   print("filter_by_offcampus")
 
 # Get current location of user and get all events whose coordinates are within
 # a TODO radius of the user
-def filter_by_nearby():
+def filter_by_nearby(search_dict):
   print("filter_by_nearby")
   # TODO JORGE
 
 # Get all events that have free food
-def filter_by_free_food():
+def filter_by_free_food(search_dict):
   print("filter_by_free_food")
   # TODO JORGE
