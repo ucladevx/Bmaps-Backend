6e4f424e4e9065163000b2864af0e6bd2e850272
Jason Xu <xu.jason.jx@gmail.com>
2018-05-04 21:38:42 -0700

    put eventbrite events in a json

--- scraping/eventbrite/eventbrite.py
+++ scraping/eventbrite/eventbrite.py
@@ -7,8 +7,13 @@ import json
 CENTER_LATITUDE = 34.070966
 CENTER_LONGITUDE = -118.445
 
+# use this as reference for now
+EVENT_FIELDS = ['name', 'category', 'place', 'description', 'start_time', 'end_time', 'event_times',
+                'attending_count', 'maybe_count', 'interested_count', 'noreply_count', 'is_canceled',
+                'ticket_uri', 'cover']
+
 days_back = 0
-days_forward = 2
+days_forward = 5
 now = datetime.datetime.now()
 past_bound = (now - datetime.timedelta(days=days_back)).strftime('%Y-%m-%dT%H:%M:%S')
 future_bound = (now + datetime.timedelta(days=days_forward)).strftime('%Y-%m-%dT%H:%M:%S')
@@ -37,12 +42,101 @@ response = session.get(
     params = search_args,
 )
 
+print('done getting events!')
 all_events = response.json().get('events')
+
+all_cat_ep = '/categories'
+cat_resp = session.get(
+    base_endpoint + all_cat_ep,
+    headers = sample_headers,
+    verify = True,
+)
+# list of dicts, 1 per category (with ID and name, full and shorthand)
+all_categories = {}
+raw_categories = cat_resp.json().get('categories')
+for raw_cat in raw_categories:
+    used_name = raw_cat['short_name'].upper()
+    if used_name == 'SPIRITUALITY':
+        used_name = 'RELIGION'
+    elif used_name == 'FOOD & DRINK':
+        used_name = 'FOOD'
+    elif used_name == 'BUSINESS':
+        used_name = 'NETWORKING'
+    elif used_name == 'ARTS':
+        used_name = 'ART'
+    elif used_name == 'FILM & MEDIA':
+        used_name = 'FILM'
+    elif used_name == 'HOME & LIFESTYLE':
+        used_name = 'HOME'
+    elif used_name == 'SPORTS & FITNESS':
+        used_name = 'SPORTS'
+    elif used_name == 'CHARITY & CAUSES':
+        used_name = 'CAUSES'
+    elif used_name == 'COMMUNITY':
+        used_name = 'NEIGHBORHOOD'
+    all_categories[raw_cat['id']] = {'full_name': raw_cat['name'], 'short_name': used_name}
+
+all_venues = {}
+
+cleaned_events = []
+
+for event_info in all_events:
+    one_event = {
+        'id': event_info.get('id', -1),
+        'name': event_info['name']['text'],
+        'description': event_info['description']['text'],
+        'interested_count': event_info.get('capacity', 0),
+        'maybe_count': 0,
+        'attending_count': 0,
+        'noreply_count': 0,
+        'is_canceled': False,
+        'cover': {
+            'source': event_info['logo']['url'] if event_info['logo'] else '<NONE>',
+            'offset_x': 0,
+            'offset_y': 0
+        },
+        'category': all_categories[event_info['category_id']]['short_name'] if event_info['category_id'] else '<NONE>',
+        'start_time': event_info['start']['local'] + '-0700'
+    }
+    if 'end' in event_info and 'local' in event_info['end']:
+        one_event['end_time'] = event_info['end']['local'] + '-0700'
+
+    venue_id = event_info['venue_id']
+    if venue_id in all_venues:
+        one_event['place'] = all_venues[venue_id]
+    else:
+        loc_resp = session.get(
+            base_endpoint + '/venues/' + venue_id,
+            headers = sample_headers,
+            verify = True,
+        )
+        loc_info = loc_resp.json()
+        new_place = {}
+        loc_details = loc_info['address']
+        new_place['location'] = {
+            'latitude': float(loc_details['latitude']),
+            'longitude': float(loc_details['longitude']),
+            'street': loc_details['address_1'],
+            'city': 'Los Angeles',
+            'state': 'CA',
+            'zipcode': loc_details['postal_code'],
+            'country': 'United States'
+        }
+        new_place['name'] = loc_info['name']
+        new_place['id'] = venue_id
+
+        all_venues[venue_id] = new_place
+        one_event['place'] = new_place
+
+    cleaned_events.append(one_event)
+
 with open('eveb.json', 'w') as f:
-    json.dump(all_events, f, sort_keys=True, indent=4, separators=(',', ': '))
+    json.dump(cleaned_events, f, sort_keys=True, indent=4, separators=(',', ': '))
+
 
 # if not all_events:
 #     print(response.json())
 # else:
 #     pprint(all_events[:3])
 #     print('# EVENTS: ' + str(len(all_events)))
+
