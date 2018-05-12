10e4b69d2c550434118e92ddbb4992fe96899807
Katrina Wijaya <katrina@thewijayas.com>
2018-05-12 06:09:11 -0700

    Revamped users/auth. TODO: testing and filters

--- src/mappening/api/users.py
+++ src/mappening/api/users.py
@@ -1,32 +1,24 @@
-# LMAO TODO FIX WOW SO MANY TODOS
+# TODO: test all the non-GET routes
 
-# Interacting with users collection in mlab
-from mappening.utils.database import users_collection
+from mappening.utils.database import users_collection, dead_users_collection
+from mappening.api.utils import user_utils
 
 from flask import Flask, jsonify, redirect, url_for, request, Blueprint
+from flask_login import current_user
 from flask_cors import CORS, cross_origin
 from datetime import datetime
-import json
-import os
 
+# Route Prefix: /api/v2/users
 users = Blueprint('users', __name__)
 
-# TODO: implement
-# Get all users with this preference
-# @users.route('/users/<preference>', methods=['GET'])
-
-# Get all preferences of this user
-# @users.route('/<int:user_id>/<preference>', methods=['GET'])
-
-# Add a preference to a specific user (WHICH?)
-# @users.route('/<int:user_id>/<preference>', methods=['POST'])
-
-# Remove a preference from a specific user
-# @users.route('/<int:user_id>/<preference>', methods=['DELETE'])
+@users.before_request
+def check_admin_permissions():
+    if not current_user.is_authenticated:
+      return "No user is logged in!"
+    if current_user.is_authenticated and not current_user.is_admin():
+      return "User does not have permissions to access/modify user data."
 
 # Get all users
-# TODO: needs authentication (logged in user should have some superpower heh)
-# https://stackoverflow.com/questions/45419802/how-to-protect-flask-restful-with-flask-user-management
 @users.route('/', methods=['GET'])
 def get_all_users():
     output = []
@@ -34,85 +26,109 @@ def get_all_users():
     users_cursor = users_collection.find({}, {'_id': False})
     if users_cursor.count() > 0:
       for user in users_cursor:
-        output.append({"user": user})
+        output.append({'user': user})
     else:
-        print('Cannot find any users!')
+        return 'Cannot find any users!'
 
     return jsonify({'users': output})
 
 # Get a specific user's information
-# TODO: needs authentication? One user shouldn't be able to access someone else's info
 # Make helper function for auth that gets info for login
 # Make separate function for frontend to use that gets info of current_user
 @users.route('/<int:user_id>', methods=['GET'])
-def get_user(user_id):
+def get_user_by_id(user_id):
     # Check that user exists
-    user = users_collection.find_one({'user_id': user_id})
-    if user != None:
-        return user
-    else:
-        return None
-
-# Update specific user's information with last logged in time
-# TODO: generalize so any new info passed in will be updated (e.g. email)
-# TODO: prtect with admin privileges
-@users.route('/<int:user_id>', methods=['PUT'])
-def update_user(userID):
-    # Check if user already exists in collection
-    user = get_user(userID)
+    user = user_utils.get_user(user_id)
     if user:
-        user['last_accessed_time'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        users_collection.replace_one({ "user_id": userID }, user.copy())
-    else:
-        return "NO_SUCH_USER"
+        return jsonify(user)
+    return "No such user with id " + str(user_id) + " found!"
+
+# Update specific user's information
+@users.route('/<int:user_id>', methods=['PUT'])
+def update_user(user_id):
+    active = request.args.get('active')
+    admin = request.args.get('admin')
+    password = request.args.get('password')
+    first_name = request.args.get('first_name')
+    last_name = request.args.get('last_name')
+    email = request.args.get('email')
+
+    # Check if user already exists in collection
+    user = user_utils.get_user(user_id)
+    if user:
+        # Update access/update/login time (in UTC I think)
+        user['account']['time_updated'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+        # Update all fields as passed in via optional parameters
+        if active and active.lower() == "true": user['account']['is_active'] = True
+        if active and active.lower() == "false": user['account']['is_active'] = False
+        if admin and admin.lower() == "true": user['account']['is_admin'] = True
+        if admin and admin.lower() == "false": user['account']['is_admin'] = False
+        if password: user['account']['password_hash'] = password  # TODO: implement hashing/salting/do this better
+        if first_name: user['personal_info']['first_name'] = first_name
+        if last_name: user['personal_info']['last_name'] = last_name
+        if email: user['personal_info']['email'] = email
+
+        # Update database entry
+        users_collection.replace_one({ "user_id": user_id }, user.copy())
+    
+    return "No such user with id " + str(user_id) + " found!"
+
+
 
 # Add a new user to users collection 
-# {user_id, user_name, user_firstname, user_lastname}
-# TODO: prtect with admin privileges
 @users.route('/', methods=['POST'])
-def add_user(userID, userName, firstName, lastName, accessToken):
-  # user_id = request.args['user_id']
-  # TODO: pass these in as request args
+def add_user_through_api():
+  user_id = request.args.get('id')
+  full_name = request.args.get('full_name', '')
+  first_name = request.args.get('first_name', '')
+  last_name = request.args.get('last_name', '')
+  email = request.args.get('email', '')
+  active = request.args.get('active', True)
+  admin = request.args.get('admin', False)
+  password = request.args.get('password', '')
+  username = request.args.get('username', '')
 
-  # Check if user already exists in collection
-  if get_user(userID):
-    return "USER_ALREADY_EXISTS"
+  if not user_id:
+    # TODO: add ID automatically, don't require it to be supplied
+    return "User ID required to add a new user"
 
-  # Insert new user
-  users_collection.insert_one({
-    "user_id": userID, 
-    "user_name": userName, 
-    "user_firstname": firstName, 
-    "user_lastname": lastName,
-    "joined_time": datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-    "last_accessed_time": datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-    "preferences": []
-  })
+  return user_utils.add_user(user_id, full_name, first_name, last_name, email, active, admin, password, username)
 
-  # Check that user was successfully added to collection
-  if get_user(userID):
-    return "ADDED_USER"
-  else:
-    return "ADDING_USER_FAILED"
-
-
-# @users.route('/deactivate/<int:user_id>', methods=['PUT']) # Deactivate user
-# TODO: Implement
+# Deactivate a user without deleting it from the database
+@users.route('/deactivate/<int:user_id>', methods=['PUT'])
+def deactivate_user(user_id):
+    # Check if user exists in collection
+    user = user_utils.get_user(user_id)
+    if user:
+        # Update status to inactive
+        user['account']['is_active'] = False
+        
+        # Update database entry
+        users_collection.replace_one({ "user_id": user_id }, user.copy())
+    
+    return "No such user with id " + str(user_id) + " found!"
 
 # Remove a user by user_id
 # Keep old user information in different database for the mems (and the info)
-# TODO: restrict accessssss
 @users.route('/<int:user_id>', methods=['DELETE'])
 def remove_user(user_id):
   # Check that user exists to remove
-  if not get_user(userID):
-    return "USER_DOES_NOT_EXIST"
+  user = user_utils.get_user(user_id)
+  if not user:
+    return "No such user with id " + str(user_id) + " found!"
   
-  # Delete user
-  users_collection.find_one_and_delete({'user_id': user_id})
+  # Delete user from OG database
+  users_collection.find_one_and_delete({'account.id': user_id})
   
   # Check that user was successfully deleted from collection
-  if get_user(userID):
-    return "USER_WAS_NOT_DELETED_BOO"
+  if user_utils.get_user(user_id):
+    return "User with id " + str(user_id) + " was not deleted successfully!"
+  
+  # Insert to database for deleted users
+  dead_users_collection.insert_one(user)
+
+  if dead_users_collection.find_one({'account.id': user_id}, {'_id': False}):
+    return "User was successfully removed from the database"
   else:
-    return "USER_WAS_DELETED_YAY"
+    return "User with id " + str(user_id) + " was successfully deleted (and saved to past users)!"
---
+++ src/mappening/api/utils/user_utils.py
@@ -0,0 +1,50 @@
+from mappening.utils.database import users_collection, dead_users_collection
+
+from flask import Flask, jsonify, redirect, url_for, request, Blueprint
+from flask_login import current_user
+from flask_cors import CORS, cross_origin
+from datetime import datetime
+
+# Get single user
+def get_user(user_id):
+    # Check that user exists
+    user = users_collection.find_one({'account.id': str(user_id)}, {'_id': False})
+    if user:
+        return user
+    return None
+
+# Add a new user to users collection 
+def add_user(user_id, full_name, first_name, last_name, email='', is_active=True, is_admin=False, password='', username=''):
+  # Check if user already exists in collection
+  if get_user(user_id):
+    return "User with id " + str(user_id) + " already exists!"
+
+  # Insert new user
+  users_collection.insert_one({
+    "account": {
+      "id": user_id,
+      "is_active": is_active,
+      "is_admin": is_admin,
+      "password_hash": password,  # TODO: we don't actually support username/passwords
+      "username": username,
+      "time_joined": datetime.now().strftime('%Y-%m-%d %H:%M:%S'), # in UTC I think
+      "time_updated": datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    },
+    "personal_info": {
+      "full_name": full_name,
+      "first_name": first_name,
+      "last_name": last_name,
+      "email": email
+    },
+    "app": {
+      "filters": [],
+      "favorites": [],
+      "past_events": []
+    }
+  })
+
+  # Check that user was successfully added to collection
+  if get_user(user_id):
+    return "User with id " + str(user_id) + " successfully added!"
+  else:
+    return "Adding user with id " + str(user_id) + " failed!"
