dab3728d689b4ad03a8c2ead45fd4811dc922df4
Sanketh Hegde <san2heg@gmail.com>
2018-11-07 19:20:15 -0800

    Add google user login authentication

---
+++ src/mappening/auth/google.py
@@ -0,0 +1,15 @@
+from mappening.utils.secrets import GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET
+from flask_oauth import OAuth
+
+oauth = OAuth();
+google_oauth = oauth.remote_app('google',
+                          base_url='https://www.google.com/accounts/',
+                          authorize_url='https://accounts.google.com/o/oauth2/auth',
+                          request_token_url=None,
+                          request_token_params={'scope': 'https://www.googleapis.com/auth/userinfo.email',
+                                                'response_type': 'code'},
+                          access_token_url='https://accounts.google.com/o/oauth2/token',
+                          access_token_method='POST',
+                          access_token_params={'grant_type': 'authorization_code'},
+                          consumer_key=GOOGLE_CLIENT_ID,
+                          consumer_secret=GOOGLE_CLIENT_SECRET)
