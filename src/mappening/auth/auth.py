3e825613d83de9df84d795b104168fe2a213c7a1
Sanketh Hegde <san2heg@gmail.com>
2018-11-18 19:39:44 -0800

    Make changes to database on login

--- src/mappening/auth/auth.py
+++ src/mappening/auth/auth.py
@@ -1,48 +1,110 @@
-from flask import Flask, redirect, url_for, session, Blueprint
-from flask_oauth import OAuth
+from mappening.api.utils import user_utils
+from mappening.api.models.user import User
+from mappening.api import users
 from google import google_oauth
 
-REDIRECT_URI = '/oauth2callback'
+from flask import Flask, jsonify, redirect, url_for, session, request, Blueprint
+from flask_login import UserMixin, LoginManager, login_required, current_user, login_user, logout_user
+from flask_cors import CORS
+from flask_oauth import OAuth
+from datetime import datetime
+import json
 
 # Route Prefix: /auth
 auth = Blueprint('auth', __name__)
 
+# Got APP_ID and APP_SECRET from Mappening app with developers.facebook.com
+DEBUG = True
+auth.debug = DEBUG
+
+# Flask-Login - configure application for login
+login_manager = LoginManager()
+
+# Required to allow Blueprints to work with flask_login
+# on_load is run when Blueprint is first registered to the app
+# Enable session protection to prevent user sessions from being stolen
+# By default is in "basic" mode. Can be set to "None or "strong"
+@auth.record_once
+def on_load(state):
+    login_manager.init_app(state.app)
+    # login_manager.session_protection = "strong"
+
+    print("Login manager set up with auth Blueprint!")
+
+# Can use user ID as remember token
+# Must change user's ID to invalidate login sessions
+# Can achieve this with session tokens rather than the user's ID
+# Would modify get_id() method to return session token rather than user's ID
+# Return None if ID is not valid, will remove ID from session and continue processing
+@login_manager.user_loader
+def user_loader(user_id):
+    db_user = user_utils.get_user(user_id)
+    if db_user:
+        return User(db_user['account']['id'], db_user['account']['is_active'], db_user['account']['is_admin'])
+    return None
+
+# TODO: move this to users but don't require admin privileges?
+@auth.route('/current', methods=['GET'])
+def get_current_user():
+    if not current_user.is_authenticated:
+      return "No user is logged in!"
+
+    user = user_utils.get_user(current_user.get_id())
+    if user:
+        return jsonify(user)
+    return "Could not get current user!"
+
 @auth.route('/')
-def index():
-    access_token = session.get('access_token')
-    if access_token is None:
-        return redirect(url_for('auth.login'))
-
-    access_token = access_token[0]
-    from urllib2 import Request, urlopen, URLError
-
-    headers = {'Authorization': 'OAuth '+access_token}
-    req = Request('https://www.googleapis.com/oauth2/v1/userinfo',
-                  None, headers)
-    try:
-        res = urlopen(req)
-    except URLError, e:
-        if e.code == 401:
-            # Unauthorized - bad token
-            session.pop('access_token', None)
-            return redirect(url_for('auth.login'))
-        return res.read()
-
-    return res.read()
+def auth_redirect():
+    if current_user.is_authenticated:
+        # return redirect(url_for('main page route'))
+        return "Already logged in!"
+    return redirect(url_for('auth.login'))
 
 @auth.route('/login')
 def login():
-    callback=url_for('auth.authorized', _external=True)
-    return google_oauth.authorize(callback=callback)
+    return google_oauth.authorize(
+      callback=url_for('auth.google_authorized',
+      next=request.args.get('next') or None, _external=True))
 
-@auth.route(REDIRECT_URI)
+# Checks whether authentication works or access is denied
+@auth.route('/login/authorized')
 @google_oauth.authorized_handler
-def authorized(resp):
-    access_token = resp['access_token']
-    session['access_token'] = access_token, ''
-    return redirect(url_for('index'))
+def google_authorized(resp):
+    if resp is None:
+        return "Access denied: reason=%s error=%s" % (
+                request.args["error_reason"],
+                request.args["error_description"]
+            )
+    session['oauth_token'] = (resp['access_token'], '')
+    session['expires'] = resp['expires_in']
+    print("Token expires in " + str(resp['expires_in']))
 
+    me = google_oauth.get('userinfo')
+    print(me.data)
+    userID = me.data['id']
+    userName = me.data['name']
+    accessToken = resp['access_token']
 
-@google_oauth.tokengetter
-def get_access_token():
-    return session.get('access_token')
+    # If user exists in collection, logs them in
+    # Otherwise, registers new user and logs them in
+    # TODO get email if we can
+    g_user = user_utils.get_user(userID)
+    if not g_user:
+        user_utils.add_user(userID, userName, me.data['given_name'], me.data['family_name'])
+        user = User(userID)
+        login_user(user)
+        return "Successfully registered new user!"
+    else:
+        users.update_user(userID)
+        user = User(userID, g_user['account']['is_active'], g_user['account']['is_admin'])
+        login_user(user)
+        return "Successfully logged in with Google!"
+
+# Log out user.
+# Will no longer be able to access any route decorated with @login_required
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return "Successfully logged out!"
--- src/mappening/auth/google.py
+++ src/mappening/auth/google.py
@@ -3,12 +3,12 @@ from flask_oauth import OAuth
 
 oauth = OAuth();
 google_oauth = oauth.remote_app('google',
-                          base_url='https://www.google.com/accounts/',
+                          base_url='https://www.googleapis.com/oauth2/v1/',
                           authorize_url='https://accounts.google.com/o/oauth2/auth',
                           request_token_url=None,
-                          request_token_params={'scope': 'https://www.googleapis.com/auth/userinfo.email',
+                          request_token_params={'scope': 'profile',
                                                 'response_type': 'code'},
-                          access_token_url='https://accounts.google.com/o/oauth2/token',
+                          access_token_url='https://www.googleapis.com/oauth2/v3/token',
                           access_token_method='POST',
                           access_token_params={'grant_type': 'authorization_code'},
                           consumer_key=GOOGLE_CLIENT_ID,
