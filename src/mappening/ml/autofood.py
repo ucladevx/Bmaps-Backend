7091213cd1682dcf52ad79d0c283fef0c7306bc7
Katrina Wijaya <katrina@thewijayas.com>
2019-05-23 02:29:28 -0700

    Removed commented import statement

--- src/mappening/ml/autofood.py
+++ src/mappening/ml/autofood.py
@@ -1,6 +1,5 @@
 # Run this file to backfill/rerun the algorithm for events_current_processed_collection
 import pickle
-# import cPickle as pickle
 import pandas as pd
 from scipy.sparse import hstack
 import itertools
